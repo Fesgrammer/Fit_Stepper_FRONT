66a86f810c2c016582e806c220d47740bb2baf87
shunpei-katayama <s2123170@cco.kanagawa-it.ac.jp>
2023-02-24 11:02:00 +0900

    テスト

--- fit-stepper/src/components/HomePage.vue
+++ fit-stepper/src/components/HomePage.vue
@@ -1,12 +1,39 @@
+<!-- HTML -->
 <template>
-  <h1>ホーム画面</h1>
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <title>FitStepper(ホーム・育成)</title>
+  </head>
+  <body>
+  <header>
+    
+  </header>
+
+  <footer>
+     <nav>
+       <ul>
+         <li><a href="#">ホーム</a></li>
+         <li><a href="#">入力</a></li>
+         <li><a href="#">履歴</a></li>
+         <li><a href="#">ランキング</a></li>
+        </ul>
+      </nav>
+    </footer>
+  </body>
+  </html>
 </template>
 
+<!-- JAVA Script -->
 <script>
 export default {
   name: "HomePage",
 };
 </script>
 
+<!-- CSS -->
 <style scoped>
+ul{
+  display: flex;
+}
 </style>
