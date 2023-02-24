501b8230bb0275aaa6e46c84408fc6d0eb7c1ce0
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-02-25 07:13:52 +0900

    タブに画像を表示するようにした。

--- fit-stepper/src/MainPage.vue
+++ fit-stepper/src/MainPage.vue
@@ -9,16 +9,24 @@
     <!--タブエリア-->
     <div class="tabArea">
       <div class="tabContents" @click="changeTab(1)">
-        <div class="tabContentsInner">ホーム</div>
+        <div class="tabContentsInner">
+          <img src="@/assets/tab/home.png" alt="ホーム" />
+        </div>
       </div>
       <div class="tabContents">
-        <div class="tabContentsInner" @click="changeTab(2)">記録</div>
+        <div class="tabContentsInner" @click="changeTab(2)">
+          <img src="@/assets/tab/regist.png" alt="記録" />
+        </div>
       </div>
       <div class="tabContents">
-        <div class="tabContentsInner" @click="changeTab(3)">履歴</div>
+        <div class="tabContentsInner" @click="changeTab(3)">
+          <img src="@/assets/tab/history.png" alt="履歴" />
+        </div>
       </div>
       <div class="tabContents">
-        <div class="tabContentsInner" @click="changeTab(4)">ランキング</div>
+        <div class="tabContentsInner" @click="changeTab(4)">
+          <img src="@/assets/tab/ranking.png" alt="ランキング" />
+        </div>
       </div>
     </div>
   </div>
@@ -57,17 +65,20 @@ div.tabArea {
   height: 100px;
   background: #cacaca;
 }
+div.tabArea img {
+  width: 80%;
+  height: auto;
+}
 div.tabContents {
   width: 25%;
   height: 100%;
-  background: #8ee99f;
   float: left;
   cursor: pointer;
+  text-align: center;
 }
 div.tabContentsInner {
   width: 90%;
   height: 90%;
-  background: #ffffff;
   margin: 0 auto;
 }
 </style>
